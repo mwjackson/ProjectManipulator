f23ca6a84d1431110d81f365d74f6a7d85f97b3d
Matt Jackson <matt.jackson@wonga.com>
2011-11-01 17:31:25 +0000

    - initial working commit

---
+++ src/deprojectreferencer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace deprojectreferencer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                new DeProjectReferencer(args[0]).Dereference();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ src/deprojectreferencer/ProjectReference.cs
@@ -0,0 +1,23 @@
+using System.Text;
+
+namespace deprojectreferencer
+{
+    public class ProjectReference
+    {
+        public string Name { get; private set; }
+        public string IncludePath { get; private set; }
+
+        public ProjectReference(string name, string includePath)
+        {
+            Name = name;
+            IncludePath = includePath;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendFormat("{0} : {1}", Name, IncludePath);
+            return sb.ToString();
+        }
+    }
+}
